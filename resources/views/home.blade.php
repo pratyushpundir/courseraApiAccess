6a2dc9f6e780d409ca13e51a7bbe2bbbcdee2616
Pratyush Pundir <pratyushpundir@icloud.com>
2016-02-13 16:58:11 -0600

    Setup a master layout with bootstrap and jquery

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.master')
+
+@section('title', 'Unnati E-Learning Data')
+
+@section('content')
+    <h1>@yield('title')</h1>
+@endsection
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>@yield('title')</title>
+        <link rel="stylesheet" href="css/vendor/bootstrap.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                
+                @yield('content')
+
+            </div>
+        </div>
+
+
+
+        <script src="/js/vendor/jquery.js"></script>
+        <script src="/js/vendor/bootstrap.js"></script>
+    </body>
+</html>
